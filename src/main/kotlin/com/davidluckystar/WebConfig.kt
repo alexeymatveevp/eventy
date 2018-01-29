c051f5310544f098efa391a11200e39b88c43f99
Alexey Matveev <david.lucky.star@gmail.com>
2018-01-29 20:14:12 +0300

    image repository config

--- src/main/kotlin/com/davidluckystar/WebConfig.kt
+++ src/main/kotlin/com/davidluckystar/WebConfig.kt
@@ -1,5 +1,6 @@
 package com.davidluckystar
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
@@ -10,11 +11,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 open class WebConfig : WebMvcConfigurerAdapter() {
+
+    @Value("\${custom.eventy.ui}")
+    val eventyUiLocation: String = ""
+
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         super.addResourceHandlers(registry)
         registry.addResourceHandler("/**")
-                .addResourceLocations("file:/C:\\dev\\workspace\\eventy-ui\\")
-                .addResourceLocations("file:/root/eventy-ui/");
+//                .addResourceLocations("file:/C:\\dev\\workspace\\eventy-ui\\")
+                .addResourceLocations("file:" + eventyUiLocation);
     }
 
     override fun addViewControllers(registry: ViewControllerRegistry?) {
