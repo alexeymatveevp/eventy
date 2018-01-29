82f88a7eef17d7e1c1124f74c021a96d566d661d
Alexey Matveev <david.lucky.star@gmail.com>
2018-01-29 19:42:36 +0300

    image repository config

--- src/main/kotlin/com/davidluckystar/service/ImageService.kt
+++ src/main/kotlin/com/davidluckystar/service/ImageService.kt
@@ -1,6 +1,7 @@
 package com.davidluckystar.service
 
 import com.davidluckystar.model.Event
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.io.File
 import java.nio.file.Files
@@ -15,14 +16,17 @@ import java.util.regex.Pattern
 @Component
 class ImageService {
 
-    val REPO: String = "C:\\Users\\david\\Pictures\\eventy"
+    @Value("\${custom.image.repo}")
+    val imageRepo: String? = ""
+
+    val DEFAULT_REPO: String = "C:\\Users\\david\\Pictures\\eventy"
     val USER_FOLDER: String = "users"
     val sdf: SimpleDateFormat = SimpleDateFormat("dd-MM-yyyy")
     val FILE_SEPARATOR = '@'
 
     fun uploadImage(creationDate: Date, filename: String, bytes: ByteArray): String {
         val containerDir = sdf.format(creationDate)
-        val dirAbsolutePath = REPO + File.separator + containerDir
+        val dirAbsolutePath = DEFAULT_REPO + File.separator + containerDir
         val dir = Paths.get(dirAbsolutePath)
         Files.createDirectories(dir)
         val imageAbsolutePath = dirAbsolutePath + File.separator + filename
@@ -36,24 +40,28 @@ class ImageService {
     }
 
     fun getImage(e: Event, filename: String): ByteArray {
-        return Files.readAllBytes(Paths.get(REPO + File.separator + sdf.format(e.creationDate) + File.separator + filename))
+        return Files.readAllBytes(Paths.get(getImageRepository() + File.separator + sdf.format(e.creationDate) + File.separator + filename))
     }
 
     fun getImage(imageRelativePath: String): ByteArray {
         val parts = imageRelativePath.split(FILE_SEPARATOR)
-        return Files.readAllBytes(Paths.get(REPO + File.separator + parts[0] + File.separator + parts[1]))
+        return Files.readAllBytes(Paths.get(getImageRepository() + File.separator + parts[0] + File.separator + parts[1]))
     }
 
     fun deleteImage(imageRelativePath: String) {
         val parts = imageRelativePath.split(FILE_SEPARATOR)
-        Files.deleteIfExists(Paths.get(REPO + File.separator + parts[0] + File.separator + parts[1]))
+        Files.deleteIfExists(Paths.get(getImageRepository() + File.separator + parts[0] + File.separator + parts[1]))
     }
 
     fun getUserImage(user: String): ByteArray {
-        val usersFolderPath = Paths.get(REPO + File.separator + USER_FOLDER)
+        val usersFolderPath = Paths.get(getImageRepository() + File.separator + USER_FOLDER)
         Files.createDirectories(usersFolderPath)
         val listFiles = usersFolderPath.toFile().listFiles()
         val userImage = listFiles.filter { f -> Pattern.compile(user + ".*").matcher(f.name).matches() }.first()
         return Files.readAllBytes(Paths.get(userImage.absolutePath))
+    }
+
+    fun getImageRepository(): String {
+        return imageRepo ?: DEFAULT_REPO
     }
 }
